924bb10361b4c9673057e71d09f40b3b9f090e0d
Capivara_Programa <153379611+CapivaraPrograma@users.noreply.github.com>
2024-04-09 13:45:25 -0300

    trabalho finalizado

--- questao14.py
+++ questao14.py
@@ -1,26 +1,9 @@
-from tkinter import *
-from tkinter import messagebox
-
-def login():
-    
-root = Tk()
-root.configure(bg="cyan4")
-
-label1 = Label(root, text='Login', bg="cyan4", fg="cyan", font=("Arial",20))
-label1.place(x=500, y=20)
-
-label2 = Label(root, text="Nome de Usuario: ", font=("Areal",20), bg="cyan4", fg="white")
-label2.place(x=310, y=190)
-
-label3 = Label(root, text="Senha do Usuario: ", font=("Areal",20), bg="cyan4", fg="white")
-label3.place(x=310, y=340)
-
-entry1 = Entry(root,font=("Areal",20))
-entry1.place(x=600, y=200)
-
-entry2 = Entry(root,font=("Areal",20), show="*")
-entry2.place(x=600, y=350)
-
-button = Button(root, text="Login", bg="cyan3", font=("Areal",15), bd=5)
-button.place(x=600,y=500)
-root.mainloop()
+# Programa para verificar se a senha de um usuário de um usuário.
+while True:
+    nome_usuario = input("Digite o seu usuário: ")
+    senha_usuario = input("Digite a sua senha: ")
+    if nome_usuario == senha_usuario:
+        print("A senha não pode ser igual ao nome de usuário.")
+    else:
+        print("O seu usuário e senha foram cadastrados.")
+        break
